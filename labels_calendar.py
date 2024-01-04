1916a8c3ce242567c6c9eaba4d2ba97407cd6611
Carlos Thompson <chlewey@gmail.com>
2024-01-03 23:59:52 -0500

    Documentation
    Documentation of auxiliary program.

--- labels_calendar.py
+++ labels_calendar.py
@@ -1,6 +1,13 @@
-
+"""Yoy shall edit and run this file to define the labels for the calendar for missing languages and holidays that fix your needs."""
 import pickle
 
+# For each language define the months and weekdays
+# use a two letter language code for common languages, and hyphen for regional variants, all lowercase.
+# 'MONTHS' is a list of month names, shall start with None as firts ([0]) element.
+# 'WEEKDAYS' is a list of week day names, shall start with Sunday as firts ([0]) element.
+# 'FORMAT_STR' is the format string used to display the date in the calendar in long format. The {0} and {1} are the day and month.
+# 'FORMAT_NUM' is the format string used to display the date in the calendar in short format. The {0} and {1} are the day and month.
+# 'ORDER' is the order of the day and month in the format string: [0,1] for day first, [1,0] for month first
 lab = {
     'es': {
         'MONTHS': [None, 'enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'],
@@ -123,29 +130,48 @@ lab = {
     }
 }
 
+# Holidays
+# use a two letter code for country, and hyphens for country subdivisions.
+# These comes in the format (month, day), numeric.
+# 'FIX' refer to fixed holidays, these are holidays that allways fall in a given date.
+# 'N:n' refer to holidyas that are alwys shifted to the next weekday n,
+#     'N:1' referes wo holidays shifted to next Monday, 'N:4' referes wo holidays shifted to next Thursday, etc.
+# 'm:n' refer to holodyas that are shifted to the next weekday n if they occur after weekday m. For example
+#     '6:1' are shifted to Monday if they occur on or after Saturday.
+# 'PASC' refer to holidays that occur n days after Easter sunday (or n days before if negatives)
+# If you want to mark special holidays, for example unoficial holidays or holidays that only apply to public sector
+#     you shall prefix them with a '@'
 hol = {
-    'co': {
+    'co': { # Colombia
+        # fixed holidays
         'FIX': [(1,1), (5,1), (7,20), (8,7), (12,8), (12,25)],
+        # holidays that are always shifted to next Monday
         'N:1': [(1,6), (3,19), (6,29), (8,15), (10,12), (11,1), (11,11)],
+        # holidays that refer to Eastern Sunday
         'PASC': [-3, -2, 0, 43, 64, 71]
     },
-    'us': {
+    'us': { # United States
         'FIX': [(1,1), (6,19), (7,4), (11,11), (12,25)],
+        # Memorial day occurs last Monday in May, this could be translated as (6,-7) for "Monday in or after June minus 7 days"
+        #    or as (5, 25) "Monday in or after May 25"
         'N:1': [(1,15), (2,15), (6,-7), (9,1), (10,8)],
+        # Forth Thursday of November referes to the Thrusday in or after November 22.
         'N:4': [(11,22)],
         'PASC': []
     },
-    'pe': {
+    'pe': { # Peru
         'FIX': [(1,1), (5,1), (6,7), (6,29), (7,23), (7,28), (7,29), (8,6), (8,30), (10,8), (11,1), (12,8), (12,9), (12,25)],
+        # Second day of Christmas and New Year are holidays for public sector, so they are marked differently.
         '@FIX': [(1,2), (12,26)],
         'PASC': [-3, -2, 0]
     },
-    'mx': {
+    'mx': { # Mexico
         'FIX': [(1,1), (5,1), (9,16), (12,25)],
         'N:1': [(2,1), (3,15), (11,15)],
+        # Holy Thursday and Long Friday are not official holidays in Mexico, but are commonly taken as holidays, they are marked differently
         '@PASC': [-3, -2]
     },
-    'se': {
+    'se': { # Sweden
         'FIX': [(1,1), (1,6), (5,1), (6,6), (12,25), (12,26)],
         'N:6': [(6,20), (10,31)],
         'PASC': [-2, 0, 1, 39, 49],
@@ -153,11 +179,12 @@ hol = {
         '@N:5': [(6,19)],
         '@PASC': [-1, 48]
     },
-    'xx': {
+    'xx': { # Hypothetical country with different holiday cases.
         'FIX': [(1,1), (5,1), (12,25)],
         'N:4': [(6,21),(11,22)],
         '6:1': [(3,19), (8,15), (9,22), (10,12)],
         'PASC': [-7, -3, -2, 0, 40, 50],
+        # If New Year, Labor Day or Christmans fall on weekend, next Monday is special holiday.
         '@6:1': [(1,1), (5,1), (12,25)],
         '@6:5': [(6,22), (11,23)],
         '@PASC': [-48, -47, 51]
